36a6946bdb3e4b952ce0a5c68553ef5cc54dd559
Peter Huppertz <peter@dnl-core.net>
2019-09-25 14:17:04 +0200

    Prepped Cheese-MVC to be ready for 3.12 prep

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,5 +1,8 @@
 package org.launchcode.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
